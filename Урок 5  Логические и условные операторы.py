c4bb5470dd2af24191e7dd21adad0d9ddc9c3f4c
Nadezhda Romanova <nadinsnigir@gmail.com>
2026-07-23 03:10:39 +0300

    Add files via upload

---
+++ Урок 5  Логические и условные операторы.py
@@ -0,0 +1,42 @@
+# Тема 2
+# Урок 5 "Логические и условные операторы"
+
+# Задача 5.1
+
+Namber = int(input ("Введите число: "))
+
+if Namber % 2 == 0:
+
+    if Namber == 0:
+        print("Нулевое число")
+
+    elif Namber / 1 < 0:
+        print("Отрицательное четное число")
+
+    # elif Namber / 1 > 0:
+    else:
+        print("Положительное четное число")
+
+else:
+    print("Число не является четным")
+
+
+
+# Задача 5.2
+
+
+word = input("Введите слово: ").lower()
+
+vowel_count = 0
+consonant_count = 0
+
+for letter in word:
+    if letter in 'aeiou':
+        vowel_count += 1
+    elif not vowel_count:
+        print("False")
+    elif letter:
+        consonant_count += 1
+
+print("Количество гласных:", vowel_count)
+print("Количество согласных:", consonant_count)
